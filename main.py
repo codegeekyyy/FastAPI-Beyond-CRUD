8b331989673fc58d0a4ffa11032b811fe714b971
project <2k23.cs2312094@gmail.com>
2026-04-15 00:16:42 +0530

    full crud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
 from typing import List
 
@@ -15,6 +16,13 @@ class Book(BaseModel):
     page_count: int
     language: str
 
+
+class UpdateBook(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str
 
 # Load books data from the JSON file
 with open("books.json", "r") as f:
@@ -39,14 +47,27 @@ async def create_a_book(book_data: Book):
 
 @app.get('/books/{book_id}')
 async def get_a_book(book_id: int) -> dict:
-    pass
+    for book in books:
+        if book['id']==book_id:
+            return book
+    raise HTTPException(status_code=404, detail="Book not found" )
 
-
-@app.put('/books/{book_id}')
-async def update_a_book(book_id: int) -> dict:
-    pass
-
+@app.patch('/books/{book_id}')
+async def update_a_book(book_id: int, book_data: UpdateBook) -> dict:
+    for book in books:
+        if book['id']==book_id:
+            book['title'] = book_data.title
+            book['author'] = book_data.author
+            book['publisher'] = book_data.publisher
+            book['page_count'] = book_data.page_count
+            book['language'] = book_data.language
+            return {"message": "Book updated successfully"}
+    raise HTTPException(status_code=404, detail="Book not found" )
 
 @app.delete('/books/{book_id}')
 async def delete_a_book(book_id: int) -> dict:
-    pass
+    for book in books:
+        if book['id']==book_id:
+            books.remove(book)
+            return {"message": "Book deleted successfully"}
+    raise HTTPException(status_code=404, detail="Book not found" )
